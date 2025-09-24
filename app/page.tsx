8c462a77d29fa7bb01fdf8e7cb39f86db978694f
Brianc1012 <caube.brian@gmail.com>
2025-09-24 19:48:10 +0800

    scrollable table

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/budget-management/adminBudgetRequest");
+  redirect("/budget-management/budgetRequest");
 }
